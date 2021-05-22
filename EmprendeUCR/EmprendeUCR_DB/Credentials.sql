0776ba147657758af5bc4a8552845a4a8b511101
jostyn delgado <jostyn.delgado@ucr.ac.cr>
2021-05-21 18:36:05 -0600

    Tables Credentials and Phones were created

---
+++ EmprendeUCR/EmprendeUCR_DB/Credentials.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Credentials]
+(
+	[User_Email] NVARCHAR(20) Primary Key NOT NULL,
+
+	[Password] VARCHAR(20) NOT NULL, 
+    CONSTRAINT [FK_Credentials_User] FOREIGN KEY ([User_Email]) REFERENCES [User]([Email]) ON DELETE CASCADE ON UPDATE CASCADE
+)
---
+++ EmprendeUCR/EmprendeUCR_DB/Phones.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Phones]
+(
+    [User_Email] VARCHAR(50) Primary Key NOT NULL, 
+    [Phone_Number] NVARCHAR(8) NOT NULL,
+    CONSTRAINT [FK_Phones_User] FOREIGN KEY ([User_Email]) REFERENCES [User]([Email]) ON DELETE CASCADE ON UPDATE CASCADE
+ 
+)
