87247a37287de58baaaca4243f4312f8c68430f0
jostyn delgado <jostyn.delgado@ucr.ac.cr>
2021-05-21 21:20:54 -0600

    Update size of user email

--- EmprendeUCR/EmprendeUCR_DB/Phones.sql
+++ EmprendeUCR/EmprendeUCR_DB/Phones.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Phones]
 (
-    [User_Email] VARCHAR(50) Primary Key NOT NULL, 
+    [User_Email] VARCHAR(100) Primary Key NOT NULL, 
     [Phone_Number] NVARCHAR(8) NOT NULL,
     CONSTRAINT [FK_Phones_User] FOREIGN KEY ([User_Email]) REFERENCES [User]([Email]) ON DELETE CASCADE ON UPDATE CASCADE
  
