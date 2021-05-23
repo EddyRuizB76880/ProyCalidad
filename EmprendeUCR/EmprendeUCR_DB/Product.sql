caa128da68fd8b5ad975284595e8712fb2256b56
dario.matamoros <dario.matamoros@ucr.ac.cr>
2021-05-23 14:27:57 +0000

    Merged in Dario (pull request #4)
    Hiding Product Id to make it an automatic increase
    
    * Hiding Product Id to make it an automatic increase
    
    Approved-by: rafael.porrasmoreno
    Approved-by: kevin.salassanchez
    Approved-by: Donaldo Salas L.

--- EmprendeUCR/EmprendeUCR_DB/Product.sql
+++ EmprendeUCR/EmprendeUCR_DB/Product.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[Product]
 (
-	[Code_ID] INT  PRIMARY KEY,
-	[Name] NVARCHAR (50) NOT NULL,
-	[Price] INT NOT NULL,
-
+    [Code_ID] INT  IDENTITY(1,1),
+    [Name] NVARCHAR (50) NOT NULL,
+    [Price] INT NOT NULL,
+    PRIMARY KEY CLUSTERED([Code_ID] ASC)
 )
-
