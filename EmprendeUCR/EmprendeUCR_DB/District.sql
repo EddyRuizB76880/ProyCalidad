7056dcd1a5efcd079b2253e934c666fada71a288
jostyn delgado <jostyn.delgado@ucr.ac.cr>
2021-05-22 19:28:47 -0600

    Create table Client

--- EmprendeUCR/EmprendeUCR_DB/District.sql
+++ EmprendeUCR/EmprendeUCR_DB/District.sql
@@ -4,5 +4,5 @@
 	[Province_Name] VARCHAR (15) NOT NULL, 
 	[Canton_Name] VARCHAR (15) NOT NULL, 
 	PRIMARY KEY ([Name],[Province_Name],[Canton_Name]),
-	CONSTRAINT [FK_District_Canton] FOREIGN KEY ([Province_Name],[Canton_Name]) REFERENCES [Canton]([ProvinceName],[Name]) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT [FK_District_Canton] FOREIGN KEY ([Province_Name],[Canton_Name]) REFERENCES [Canton]([Province_Name],[Name]) ON DELETE CASCADE ON UPDATE CASCADE
 )
