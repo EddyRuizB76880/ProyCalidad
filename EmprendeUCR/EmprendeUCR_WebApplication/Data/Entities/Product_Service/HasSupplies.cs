969381a1e8d3d69db317333a0227bd9e02794b48
AndresCR <andrescr94@outlook.com>
2021-07-11 14:31:50 -0600

    add communication child component to parent statuses

---
+++ EmprendeUCR/EmprendeUCR_WebApplication/Data/Entities/Product_Service/HasSupplies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmprendeUCR_WebApplication.Data.Entities
+{
+    public class HasSupplies
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Offer_ID { get; set; }
+        public DateTime Initial_Date { get; set; }
+        public DateTime Expire_Date { get; set; }
+        public string Offer_Description { get; set; }
+        [Required]
+        public int Discount { get; set; }
+    }
+}
