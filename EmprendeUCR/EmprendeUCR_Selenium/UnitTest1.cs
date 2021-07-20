255d1268109542f5d0e638b9501f7dc6bbd1cb37
AndresCR <andrescr94@outlook.com>
2021-07-20 11:50:39 -0600

    PIIB12021-343 Modify offers with the allies -> PIIB12021-471
    Driver: Andres Chaves
    Copilot: Rafael Porras
    
    Commit #2
    UI tests with selenium web driver
    
    % accomplishment-> 5
    
    % contributors-> 50/50

---
+++ EmprendeUCR/EmprendeUCR_Selenium/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading.Tasks;
+
+namespace ECCI_IS_Lab01_WebApp.Selenium
+{
+    [TestClass]
+    public class Selenium
+    {
+        IWebDriver driver;
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            if (driver != null)
+                driver.Quit();
+        }
+        [TestMethod]
+        public async Task PruebaIngresoChrome()
+        {
+            ///Arrange
+            /// Crea el driver de Chrome
+            driver = new ChromeDriver();
+            /// Pone la pantalla en full screen
+            driver.Manage().Window.Maximize();
+            ///Act
+            /// Se va a la URL de la aplicacion
+            driver.Url = "https://localhost:44366/";
+            ///Assert
+            await Task.Delay(1000);
+            var element = driver.FindElement(By.XPath("//a[@href='/Offers']"));
+            Assert.AreEqual(element.Text, "OFERTAS");
+        }
+    }
+}
