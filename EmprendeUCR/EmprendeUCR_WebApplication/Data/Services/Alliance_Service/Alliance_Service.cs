6f15ab27b1a6cdd7922be9979c44949ec442f5ee
Kevin_Salas_Sanchez <KEVIN.SALASSANCHEZ@ucr.ac.cr>
2021-07-25 00:38:56 -0600

    PIIB12021-147 : create alliance
       Add_Alliance_Service
    
      Add_Alliance_Entitie

---
+++ EmprendeUCR/EmprendeUCR_WebApplication/Data/Services/Alliance_Service/Alliance_Service.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmprendeUCR_WebApplication.Data.Entities;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EmprendeUCR_WebApplication.Data;
+using EmprendeUCR_WebApplication.Data.Entities.ContextShop;
+
+namespace EmprendeUCR_WebApplication.Data.Services.Alliance_Service
+{
+    
+    public class Alliance_Service: PageModel
+    {
+        private readonly EmprendeUCR_WebApplication.Data.Contexts.SqlServerDbContext _context;
+
+
+        public Alliance_Service(EmprendeUCR_WebApplication.Data.Contexts.SqlServerDbContext context) {
+
+            _context = context;
+
+        }
+
+
+        public async Task<IList<Alliance>> GetAsync()    // Enlista Alianzas 
+        {
+            return await _context.Alliance.ToListAsync();
+        }
+
+
+        public async Task<bool> InsertAllianceServiceAsync(Alliance alliance) // Agrega Alianzas
+        {
+
+            Alliance obj = alliance;
+            await _context.Alliance.AddAsync(obj);
+            await _context.SaveChangesAsync();
+            Console.WriteLine(obj);
+            return true;
+        }
+
+
+
+        public void delete_Alliace(Alliance alliance) {
+
+            _context.Alliance.Remove(alliance);
+            _context.SaveChanges();
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+}
